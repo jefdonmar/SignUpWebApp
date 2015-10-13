59783bb6a3ed774b2a07efbbe156456e2152c7c9
Jeffrey Martín <jefdonmar@gmail.com>
2015-10-13 16:57:20 -0400

    Finished the Person A JS Part I

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,35 @@
 
 (function () {
 
-  console.log('It Works!');
+  var url = "http://json-data.herokuapp.com/forms";
+  var promise = $.getJSON(url);
+
+  promise.then(function (res) {
+    doSomething(res);
+  });
+
+  var textInput = function textInput(obj) {
+    var templateInput = '\n    <div class="text-input">\n      <input type = "' + obj.type + '" placeholder = "' + obj.label + '" id = "' + obj.id + '">\n    </div>\n    ';
+    return templateInput;
+  };
+
+  var textSelect = function textSelect(obj) {
+    var templateSelect = '\n    <div class="text-select">\n      <select type = "' + obj.type + '" placeholder = "' + obj.label + '" id = "' + obj.id + '">\n        <option value ="">Select Language...</option>\n        <option value ="' + obj.options[0].value + '" label ="' + obj.options[0].label + '"></option>\n        <option value ="' + obj.options[1].value + '" label ="' + obj.options[1].label + '"></option>\n        <option value ="' + obj.options[2].value + '" label ="' + obj.options[2].label + '"></option>\n        <option value ="' + obj.options[3].value + '" label ="' + obj.options[3].label + '"></option>\n        <option value ="' + obj.options[4].value + '" label ="' + obj.options[4].label + '"></option>\n      </select>\n    </div>\n    ';
+    return templateSelect;
+  };
+
+  //Our do something function
+  var doSomething = function doSomething(arr) {
+    _.each(arr, function (item) {
+      var htmlBlock = undefined;
+
+      if (item.type == 'text' || item.type == 'email' || item.type == 'tel' || item.type == 'textarea') {
+        htmlBlock = textInput(item);
+      } else if (item.type == "select") {
+        htmlBlock = textSelect(item);
+      }
+
+      $('form').append(htmlBlock);
+    });
+  };
 })();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,50 @@
 (function () {
 
-  console.log('It Works!');
+  let url = "http://json-data.herokuapp.com/forms";
+  let promise = $.getJSON(url);
+
+  promise.then ( function (res) {
+    doSomething(res);
+  });
+
+  let textInput = function(obj){
+    let templateInput =`
+    <div class="text-input">
+      <input type = "${ obj.type }" placeholder = "${ obj.label }" id = "${ obj.id }">
+    </div>
+    `;
+    return templateInput;
+  }  ;
+
+  let textSelect = function(obj){
+    let templateSelect =`
+    <div class="text-select">
+      <select type = "${ obj.type }" placeholder = "${ obj.label }" id = "${ obj.id }">
+        <option value ="">Select Language...</option>
+        <option value ="${ obj.options[0].value }" label ="${ obj.options[0].label }"></option>
+        <option value ="${ obj.options[1].value }" label ="${ obj.options[1].label }"></option>
+        <option value ="${ obj.options[2].value }" label ="${ obj.options[2].label }"></option>
+        <option value ="${ obj.options[3].value }" label ="${ obj.options[3].label }"></option>
+        <option value ="${ obj.options[4].value }" label ="${ obj.options[4].label }"></option>
+      </select>
+    </div>
+    `;
+    return templateSelect;
+  }  ;
+  
+  //Our do something function 
+  let doSomething = function (arr) {
+    _.each(arr,function(item){
+      let htmlBlock;
+
+      if (item.type == 'text' || item.type == 'email' || item.type == 'tel' || item.type == 'textarea') {
+        htmlBlock = textInput(item);
+      } else if (item.type == "select") {
+        htmlBlock = textSelect(item);
+      }
+
+      $('form').append(htmlBlock);
+    });
+  };
 
 }());
